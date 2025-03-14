8eb4543b2be50062e63328c5ee2afffd41039e9f
Iagoba-png <iago.alcoba@udc.es>
2025-03-14 19:23:14 +0100

    Final Dynamic pa entregar

--- cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
-  "C:/Users/EQUIPO/CLionProjects/P1_PROII/bin/main.exe"
-  "C:/Users/EQUIPO/CLionProjects/P1_PROII/bin/main.exe.manifest"
-  "C:/Users/EQUIPO/CLionProjects/P1_PROII/bin/main.pdb"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/main.exe"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/main.exe.manifest"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/main.pdb"
   "CMakeFiles/main.dir/dynamic_list.c.obj"
   "CMakeFiles/main.dir/dynamic_list.c.obj.d"
   "CMakeFiles/main.dir/main.c.obj"
--- cmake-build-debug/CMakeFiles/test_lists.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/test_lists.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
-  "C:/Users/EQUIPO/CLionProjects/P1_PROII/bin/test_lists.exe"
-  "C:/Users/EQUIPO/CLionProjects/P1_PROII/bin/test_lists.exe.manifest"
-  "C:/Users/EQUIPO/CLionProjects/P1_PROII/bin/test_lists.pdb"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/test_lists.exe"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/test_lists.exe.manifest"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/test_lists.pdb"
   "CMakeFiles/test_lists.dir/dynamic_list.c.obj"
   "CMakeFiles/test_lists.dir/dynamic_list.c.obj.d"
   "CMakeFiles/test_lists.dir/test.c.obj"
