45dbc242abb7307004dc204c12b590c5a4c1a369
Iagoba-png <iago.alcoba@udc.es>
2025-03-14 19:24:30 +0100

    Revision static

---
+++ cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/main.exe"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/main.exe.manifest"
+  "C:/Users/122124/CLionProjects/P1_PROII/bin/main.pdb"
+  "CMakeFiles/main.dir/main.c.obj"
+  "CMakeFiles/main.dir/main.c.obj.d"
+  "CMakeFiles/main.dir/static_list.c.obj"
+  "CMakeFiles/main.dir/static_list.c.obj.d"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
